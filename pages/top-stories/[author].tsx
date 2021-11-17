b00122c49f0c4f0b240674712fe335fe787bf877
Charly_Angel <c.a.a.a.503@gmail.com>
2021-11-16 21:37:19 -0600

    feat: página de error persolazido

--- pages/top-stories/[author].tsx
+++ pages/top-stories/[author].tsx
@@ -11,6 +11,7 @@ import { AuthorCard } from '@components/AuthorCard'
 import { getAuthorList, getPlantListByAuthor, QueryStatus } from '@api'
 import { IGetPlantListByAuthorQueryVariables } from '@api/generated/graphql'
 import { useRouter } from 'next/dist/client/router'
+import ErrorPage from '../_error'
 
 type TopStoriesPageProps = {
   authors: Author[]
@@ -72,20 +73,7 @@ export default function TopStories({
     status === 'error'
   ) {
     return (
-      <Layout>
-        <main className="pt-10 px-6">
-          <div className="pb-16">
-            <Typography variant="h2">Huh, algo no está bien 🙇‍♀️</Typography>
-          </div>
-          <article>
-            <Alert severity="error">
-              {status === 'error'
-                ? 'Hubo un error consultando la información. Inspeccionar el request en la pestaña Network de DevTools podría dar más información'
-                : 'No se encontró la información. ¿Olvidaste configurar el contenido en Contentful?'}
-            </Alert>
-          </article>
-        </main>
-      </Layout>
+      <ErrorPage message="There is no information available. Did you forget to set up your Contenful space's content?" />
     )
   }
 
